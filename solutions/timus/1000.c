24132b29b08629a024fb24a4ffe5d1cc70a513b4
yangyanzhan <yangyanzhan@gmail.com>
2016-10-21 15:54:23 +0800

    solution for timus problem no 1000

---
+++ solutions/timus/1000.c
@@ -0,0 +1,25 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge timus
+ * @problem_id 1000
+ * @problem_address http://acm.timus.ru/problem.aspx?space=1&num=1000
+ **/
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d", a + b);
+    return 0;
+}
