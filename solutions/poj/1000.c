4b57fe70c9e958c09fe489efe782c013e73610ca
yangyanzhan <yangyanzhan@gmail.com>
2016-10-23 13:26:58 +0800

    solution for poj problem no 1000

---
+++ solutions/poj/1000.c
@@ -0,0 +1,24 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge poj
+ * @problem_id 1000
+ * @problem_address http://poj.org/problem?id=1000
+ **/
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
