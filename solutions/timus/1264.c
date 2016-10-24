4416059978d90b0b77618b208cdf5c7f6f5fce3f
yangyanzhan <yangyanzhan@gmail.com>
2016-10-24 16:59:58 +0800

    solution for timus problem no 1264

---
+++ solutions/timus/1264.c
@@ -0,0 +1,24 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge timus
+ * @problem_id 1264
+ * @problem_address http://acm.timus.ru/problem.aspx?space=1&num=1264
+ **/
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    printf("%d\n", n * (m + 1));
+    return 0;
+}
