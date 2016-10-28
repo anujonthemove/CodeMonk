e1bcedb1c767dc95a859958fe334915b38c4d320
yangyanzhan <yangyanzhan@gmail.com>
2016-10-28 23:08:02 +0800

    solution for hackerearth problem id roys-life-cycle-44

---
+++ solutions/hackerearth/roys-life-cycle-44.c
@@ -0,0 +1,54 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerearth
+ * @problem_id roys-life-cycle-44
+ * @problem_address https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/roys-life-cycle-44/
+ **/
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LEN 1440
+
+int count(char *line){
+    int n = strlen(line);
+    int best = 0, c = 0;
+    for(int i = 0; i <= n; i++){
+        char now = line[i];
+        if(now == 'C'){
+            c += 1;
+        }else{
+            if(c > best){
+                best = c;
+            }
+            c = 0;
+        }
+    }
+    return best;
+}
+
+int main(int argc, char *argv[]){
+    int n;
+    scanf("%d", &n);
+    char *line = (char *)malloc((LEN + 1) * sizeof(char));
+    char *word = (char *)malloc((n * LEN + 1) * sizeof(char));
+    int best = 0;
+    for(int i = 0; i < n; i++){
+        scanf("%s", line);
+        strcpy(word + i * LEN, line);
+        int c = count(line);
+        if(c > best){
+            best = c;
+        }
+    }
+    printf("%d %d\n", best, count(word));
+    return 0;
+}
