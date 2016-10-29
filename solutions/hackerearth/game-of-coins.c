dc771131cc814b9e22bab35b0e509296e9f246e9
yangyanzhan <yangyanzhan@gmail.com>
2016-10-29 11:32:55 +0800

    solution for hackerearth problem id game-of-coins

---
+++ solutions/hackerearth/game-of-coins.c
@@ -0,0 +1,26 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerearth
+ * @problem_id game-of-coins
+ * @problem_address https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/game-of-coins/
+ **/
+
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]){
+    int T;
+    scanf("%d", &T);
+    for(int t = 0; t < T; t++){
+        printf("Alice\n");
+    }
+    return 0;
+}
