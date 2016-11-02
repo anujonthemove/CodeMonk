536a5b5911ddbe94557bbd8bb741ecba90cfde73
yangyanzhan <yangyanzhan@gmail.com>
2016-11-02 23:37:44 +0800

    solution for hackerrank problem id solve-me-first

---
+++ solutions/hackerrank/solve-me-first.cpp
@@ -0,0 +1,30 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id solve-me-first
+ * @problem_address https://www.hackerrank.com/challenges/solve-me-first
+ **/
+
+#include <set>
+#include <map>
+#include <cmath>
+#include <regex>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+    return 0;
+}
