760fdaa6572678bf5696037b5d5fefe60cec72a8
yangyanzhan <yangyanzhan@gmail.com>
2016-11-02 23:05:14 +0800

    solution for hackerrank problem id 30-hello-world

---
+++ solutions/hackerrank/30-hello-world.cpp
@@ -0,0 +1,30 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id 30-hello-world
+ * @problem_address https://www.hackerrank.com/challenges/30-hello-world
+ **/
+
+#include <set>
+#include <map>
+#include <cmath>
+#include <regex>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    string line;
+    getline(cin, line);
+    cout << "Hello, World." << endl;
+    cout << line << endl;
+    return 0;
+}
