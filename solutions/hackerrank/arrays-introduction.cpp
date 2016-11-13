6630d6b65e4712ce44f2d0aface09982d6d88fcb
yangyanzhan <yangyanzhan@gmail.com>
2016-11-14 01:16:47 +0800

    solution for hackerrank problem id arrays-introduction

---
+++ solutions/hackerrank/arrays-introduction.cpp
@@ -0,0 +1,41 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id arrays-introduction
+ * @problem_address https://www.hackerrank.com/challenges/arrays-introduction
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+    for (vector<int>::reverse_iterator it = nums.rbegin(); it != nums.rend();
+         it++) {
+        cout << *it << " ";
+    }
+    return 0;
+}
