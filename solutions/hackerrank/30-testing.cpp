64c719cb67148ed1cf5bc649405713c8a57825e7
yangyanzhan <yangyanzhan@gmail.com>
2016-11-14 00:25:38 +0800

    solution for hackerrank problem id 30-testing

---
+++ solutions/hackerrank/30-testing.cpp
@@ -0,0 +1,40 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id 30-testing
+ * @problem_address https://www.hackerrank.com/challenges/30-testing
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "5" << endl;
+    cout << "5 3" << endl;
+    cout << "-1 90 999 100 0" << endl;
+    cout << "4 2" << endl;
+    cout << "0 -1 2 1" << endl;
+    cout << "3 3" << endl;
+    cout << "-1 0 1" << endl;
+    cout << "6 1" << endl;
+    cout << "-1 0 1 -1 2 3" << endl;
+    cout << "7 3" << endl;
+    cout << "-1 0 1 2 3 4 5" << endl;
+    return 0;
+}
