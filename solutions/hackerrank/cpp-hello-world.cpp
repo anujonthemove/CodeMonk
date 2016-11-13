32fb80a0b3e9d18122b8a20744af6fe891448637
yangyanzhan <yangyanzhan@gmail.com>
2016-11-14 00:59:03 +0800

    solution for hackerrank problem id cpp-hello-world

---
+++ solutions/hackerrank/cpp-hello-world.cpp
@@ -0,0 +1,30 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id cpp-hello-world
+ * @problem_address https://www.hackerrank.com/challenges/cpp-hello-world
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
