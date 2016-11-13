7b76e0fc117f9a9424a1d2a2f6423d2397345fe0
yangyanzhan <yangyanzhan@gmail.com>
2016-11-14 01:00:13 +0800

    solution for hackerrank problem id cpp-input-and-output

---
+++ solutions/hackerrank/cpp-input-and-output.cpp
@@ -0,0 +1,32 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id cpp-input-and-output
+ * @problem_address https://www.hackerrank.com/challenges/cpp-input-and-output
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a + b + c) << endl;
+    return 0;
+}
