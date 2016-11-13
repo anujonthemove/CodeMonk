72cda4d7ee5842c99b8d5d61fd5f68f06985f5ec
yangyanzhan <yangyanzhan@gmail.com>
2016-11-14 01:04:25 +0800

    solution for hackerrank problem id c-tutorial-basic-data-types

---
+++ solutions/hackerrank/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,38 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id c-tutorial-basic-data-types
+ * @problem_address
+ *https://www.hackerrank.com/challenges/c-tutorial-basic-data-types
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf\n", a, b, c, d, e, f);
+    return 0;
+}
