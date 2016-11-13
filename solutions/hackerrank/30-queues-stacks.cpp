a00c0d09cf5ba61467cd96be44175543e9e82b05
yangyanzhan <yangyanzhan@gmail.com>
2016-11-13 23:29:27 +0800

    solution for hackerrank problem id 30-queues-stacks

---
+++ solutions/hackerrank/30-queues-stacks.cpp
@@ -0,0 +1,81 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id 30-queues-stacks
+ * @problem_address https://www.hackerrank.com/challenges/30-queues-stacks
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  private:
+    queue<char> q;
+    stack<char> s;
+
+  public:
+    void pushCharacter(char ch) { s.push(ch); }
+    void enqueueCharacter(char ch) { q.push(ch); }
+    char popCharacter() {
+        char ch = s.top();
+        s.pop();
+        return ch;
+    }
+    char dequeueCharacter() {
+        char ch = q.front();
+        q.pop();
+        return ch;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    // read the string s.
+    string s;
+    getline(cin, s);
+
+    // create the Solution class object p.
+    Solution obj;
+
+    // push/enqueue all the characters of string s to stack.
+    for (int i = 0; i < s.length(); i++) {
+        obj.pushCharacter(s[i]);
+        obj.enqueueCharacter(s[i]);
+    }
+
+    bool isPalindrome = true;
+
+    // pop the top character from stack.
+    // dequeue the first character from queue.
+    // compare both the characters.
+    for (int i = 0; i < s.length() / 2; i++) {
+        if (obj.popCharacter() != obj.dequeueCharacter()) {
+            isPalindrome = false;
+
+            break;
+        }
+    }
+
+    // finally print whether string s is palindrome or not.
+    if (isPalindrome) {
+        cout << "The word, " << s << ", is a palindrome.";
+    } else {
+        cout << "The word, " << s << ", is not a palindrome.";
+    }
+    return 0;
+}
