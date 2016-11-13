ce4348bd68240c63af6146dad1c22eccc73d53b8
yangyanzhan <yangyanzhan@gmail.com>
2016-11-13 21:58:27 +0800

    solution for hackerrank problem id 30-loops

---
+++ solutions/hackerrank/30-loops.cpp
@@ -0,0 +1,33 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id 30-loops
+ * @problem_address https://www.hackerrank.com/challenges/30-loops
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << (i * n) << endl;
+    }
+    return 0;
+}
