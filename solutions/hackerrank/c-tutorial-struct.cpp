ce24d596d6dac0762d274e0c4da77669c5922c00
yangyanzhan <yangyanzhan@gmail.com>
2016-11-17 01:07:47 +0800

    solution for hackerrank problem id c-tutorial-struct

---
+++ solutions/hackerrank/c-tutorial-struct.cpp
@@ -0,0 +1,42 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id c-tutorial-struct
+ * @problem_address https://www.hackerrank.com/challenges/c-tutorial-struct
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " "
+         << st.standard;
+
+    return 0;
+}
