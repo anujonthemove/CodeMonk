e2b189129b5aba0cebe11ac54241263341b6be54
yangyanzhan <yangyanzhan@gmail.com>
2016-11-21 00:37:37 +0800

    solution for hackerrank problem id maximum-draws

---
+++ solutions/hackerrank/maximum-draws.cpp
@@ -0,0 +1,36 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id maximum-draws
+ * @problem_address https://www.hackerrank.com/challenges/maximum-draws
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int n;
+        cin >> n;
+        cout << (n + 1) << endl;
+    }
+    return 0;
+}
