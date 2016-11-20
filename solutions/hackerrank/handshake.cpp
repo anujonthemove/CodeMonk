a3ba107ceb228fc9a3ee6c46136ae9983a83ba43
yangyanzhan <yangyanzhan@gmail.com>
2016-11-21 00:39:07 +0800

    solution for hackerrank problem id handshake

---
+++ solutions/hackerrank/handshake.cpp
@@ -0,0 +1,36 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id handshake
+ * @problem_address https://www.hackerrank.com/challenges/handshake
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        long long n;
+        cin >> n;
+        cout << (n * (n - 1) / 2) << endl;
+    }
+    return 0;
+}
