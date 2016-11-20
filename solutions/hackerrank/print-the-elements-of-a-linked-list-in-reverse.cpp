9ef0b3c768e6b53afd85768aa4e0cfb0ca191925
yangyanzhan <yangyanzhan@gmail.com>
2016-11-20 18:52:34 +0800

    solution for hackerrank problem id print-the-elements-of-a-linked-list-in-reverse

---
+++ solutions/hackerrank/print-the-elements-of-a-linked-list-in-reverse.cpp
@@ -0,0 +1,35 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge hackerrank
+ * @problem_id print-the-elements-of-a-linked-list-in-reverse
+ * @problem_address
+ *https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void ReversePrint(Node *head) {
+    if (head == NULL)
+        return;
+    ReversePrint(head->next);
+    cout << head->data << endl;
+}
+
+int main(int argc, char *argv[]) { return 0; }
