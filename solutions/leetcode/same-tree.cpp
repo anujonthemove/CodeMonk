a518104dedce3c6e3ce1f7c3d955d2efdfdab512
yangyanzhan <yangyanzhan@gmail.com>
2016-12-09 11:35:49 +0800

    solution for leetcode problem id same-tree

---
+++ solutions/leetcode/same-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id same-tree
+ * @problem_address https://leetcode.com/problems/same-tree/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if ((p && !q) || (!p && q)) {
+            return false;
+        }
+        if (!p && !q) {
+            return true;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
