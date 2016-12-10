6ef587950b5649ce6758307681212cb07e662cb7
yangyanzhan <yangyanzhan@gmail.com>
2016-12-10 22:38:08 +0800

    solution for leetcode problem id reverse-string

---
+++ solutions/leetcode/reverse-string.cpp
@@ -0,0 +1,35 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id reverse-string
+ * @problem_address https://leetcode.com/problems/reverse-string/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    string reverseString(string s) {
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
