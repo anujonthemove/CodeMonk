f2d17e8915f55e15db6105b73bf7952a049f304a
yangyanzhan <yangyanzhan@gmail.com>
2016-12-11 00:07:07 +0800

    solution for leetcode problem id add-digits

---
+++ solutions/leetcode/add-digits.cpp
@@ -0,0 +1,45 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id add-digits
+ * @problem_address https://leetcode.com/problems/add-digits/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int sum(int num) {
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        return res;
+    }
+    int addDigits(int num) {
+        while (num >= 10) {
+            num = sum(num);
+        }
+        return num;
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
