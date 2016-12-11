5dc171ca4024ad22e3a712f19d786f9dcdb988bc
yangyanzhan <yangyanzhan@gmail.com>
2016-12-11 19:23:44 +0800

    solution for leetcode problem id valid-anagram

---
+++ solutions/leetcode/valid-anagram.cpp
@@ -0,0 +1,36 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id valid-anagram
+ * @problem_address https://leetcode.com/problems/valid-anagram/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
