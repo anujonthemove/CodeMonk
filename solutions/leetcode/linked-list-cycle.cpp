35913b2a734ef0d3d0ca3f5a3b710acb83301b8a
yangyanzhan <yangyanzhan@gmail.com>
2016-12-11 23:18:32 +0800

    solution for leetcode problem id linked-list-cycle

---
+++ solutions/leetcode/linked-list-cycle.cpp
@@ -0,0 +1,42 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id linked-list-cycle
+ * @problem_address https://leetcode.com/problems/linked-list-cycle/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
