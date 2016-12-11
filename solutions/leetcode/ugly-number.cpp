17f4262a7fe2dc673280e608313ace1a86b815f4
yangyanzhan <yangyanzhan@gmail.com>
2016-12-11 22:18:56 +0800

    solution for leetcode problem id ugly-number

---
+++ solutions/leetcode/ugly-number.cpp
@@ -0,0 +1,46 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id ugly-number
+ * @problem_address https://leetcode.com/problems/ugly-number/
+ **/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+};
+
+int main(int argc, char *argv[]) { return 0; }
