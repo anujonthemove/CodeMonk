5144baeedfbe47bfd0435cbc5b540e764df83df9
yangyanzhan <yangyanzhan@gmail.com>
2016-12-13 10:43:07 +0800

    solution for leetcode problem id arithmetic-slices

---
+++ solutions/leetcode/arithmetic-slices.cpp
@@ -0,0 +1,29 @@
+/**
+ * @author yangyanzhan
+ * @email yangyanzhan@gmail.com
+ * @homepage http://www.yangyanzhan.com
+ * @github_project https://github.com/yangyanzhan/CodeMonk
+ * @online_judge leetcode
+ * @problem_id arithmetic-slices
+ * @problem_address https://leetcode.com/problems/arithmetic-slices/
+ **/
+
+#include <set>
+#include <map>
+#include <cmath>
+#include <regex>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
